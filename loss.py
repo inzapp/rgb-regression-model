36c74ab18dfad2c34ecb6f80431dd2b8f715e67c
inzapp <inzapp@naver.com>
2021-10-25 14:49:37 +0900

    c

--- loss.py
+++ loss.py
@@ -19,29 +19,17 @@ class RGBLoss(tf.keras.losses.Loss):
         while tf.constant(True, dtype=tf.dtypes.bool):
             confidence_true = y_true[:, confidence_index]
             confidence_pred = y_pred[:, confidence_index]
-            # cur_confidence_loss = tf.reduce_sum(tf.square(confidence_true - confidence_pred)) / batch_size_f
-            cur_confidence_loss = tf.reduce_sum(-tf.math.log(1.0 - tf.abs(confidence_true - confidence_pred))) / batch_size_f
-            confidence_loss += cur_confidence_loss
+            confidence_loss += tf.reduce_sum(-tf.math.log(1.0 - tf.abs(confidence_true - confidence_pred))) / batch_size_f
 
             rgb_mask = tf.reshape(confidence_true, (batch_size, 1))
             rgb_mask = tf.repeat(rgb_mask, 3, axis=-1)
 
             rgb_true = y_true[:, confidence_index + 1: confidence_index + 4]
             rgb_pred = y_pred[:, confidence_index + 1: confidence_index + 4]
-            # cur_rgb_loss = tf.reduce_sum(tf.square((rgb_true * rgb_mask) - (rgb_pred * rgb_mask))) / batch_size_f
-            cur_rgb_loss = tf.reduce_sum(-tf.math.log(1.0 - tf.abs(rgb_true - rgb_pred))) / batch_size_f
-            rgb_loss += cur_rgb_loss
+            rgb_loss += tf.reduce_sum(-tf.math.log(1.0 - tf.abs(rgb_true - rgb_pred))) / batch_size_f
 
             confidence_index = tf.add(confidence_index, tf.constant(4, dtype=tf.dtypes.int32))
             if tf.greater_equal(confidence_index, num_output_node):
                 break
 
         return confidence_loss + (rgb_loss * self.lambda_rgb)
-
-    # def call(self, y_true, y_pred):
-    #     y_pred = convert_to_tensor_v2(y_pred)
-    #     y_true = tf.cast(y_true, y_pred.dtype)
-    #     loss = tf.square(y_true - y_pred)
-    #     loss = tf.reduce_mean(loss, axis=0)
-    #     loss = tf.reduce_sum(loss)
-    #     return loss 
