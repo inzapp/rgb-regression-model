fbfc6581ab68f448aaf8010fa0c90d656dfd9fd9
inzapp <inzapp@naver.com>
2023-01-22 15:23:30 +0900

    update loss function to focal ale with l2

--- loss.py
+++ loss.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
+from ale import AbsoluteLogarithmicError
+
+
+focal_ale = AbsoluteLogarithmicError(gamma=2.0, label_smoothing=0.1)
 
 
 def confidence_rgb_loss(y_true, y_pred):
@@ -13,7 +16,7 @@ def confidence_rgb_loss(y_true, y_pred):
     while tf.constant(True, dtype=tf.dtypes.bool):
         confidence_true = y_true[:, confidence_index]
         confidence_pred = y_pred[:, confidence_index]
-        confidence_loss += tf.reduce_sum(-K.log(1.0 + K.epsilon() - K.abs(confidence_true - confidence_pred))) / batch_size_f
+        confidence_loss += tf.reduce_sum(focal_ale(confidence_true, confidence_pred)) / batch_size_f
 
         rgb_mask = tf.reshape(confidence_true, (batch_size, 1))
         rgb_mask = tf.repeat(rgb_mask, 3, axis=-1)
@@ -47,5 +50,5 @@ def yuv_loss(y_true, y_pred):
 
     yuv_true = tf.concat([yuv_y_true, yuv_u_true, yuv_v_true], axis=-1)
     yuv_pred = tf.concat([yuv_y_pred, yuv_u_pred, yuv_v_pred], axis=-1)
-    return -K.log(1.0 + K.epsilon() - K.abs(yuv_true - yuv_pred))
+    return tf.square(yuv_true - yuv_pred)
 
